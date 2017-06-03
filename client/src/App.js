d47de37b61a1e65e2aaa8350c0021820a9e52fe8
DTaitt <donovan.taitt@yahoo.com>
2017-06-03 14:12:34 -0400

    created LocationInput component

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,74 @@ import './App.css';
 import Header from './components/Header.jsx'
 import LocationInput from './components/LocationInput.jsx'
 import Footer from './components/Footer.jsx'
+import CurrentInfo from './components/CurrentInfo.jsx'
 
 class App extends Component {
+
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     locationLat: null,
+  //     locationLong: null,
+  //   }
+
+  //   this.setLocation = this.setLocation.bind(this);
+  // }
+
+  // setLocation(lat, long) {
+  //   this.setState({
+  //     locationLat: lat,
+  //     locationLong: long,
+  //   })
+  //   console.log(`${this.state.locationLat} from app`)
+  // }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      locationValue: null,
+      locationLat: null,
+      locationLong: null,
+    }
+
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleLocationSubmit = this.handleLocationSubmit.bind(this);
+  }
+
+  handleLocationSubmit(event) {
+    event.preventDefault();
+
+    console.log(this.state.locationValue)
+
+    //converts address to lat and long for use in Dark Sky API
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${ this.state.locationValue }&key=AIzaSyB_hFnwO_brmJWv01IMy5IaVBd8MJgSIZw
+`)
+    .then((res) => {
+      return res.json()
+    })
+    .then((jsonRes) => {
+      this.setState({
+        locationLat: jsonRes.results[0].geometry.location.lat,
+        locationLong: jsonRes.results[0].geometry.location.lng,
+      })
+      console.log(this.state.locationLat);
+      console.log(this.state.locationLong);
+    })
+  }
+
+  handleLocationChange(event) {
+    this.setState({
+      locationValue: event.target.value,
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
         <main>
-          <LocationInput />
+          <LocationInput handleLocationChange={ this.handleLocationChange } handleLocationSubmit={ this.handleLocationSubmit } />
+          <CurrentInfo />
         </main>
         <Footer />
       </div>
--- client/src/components/LocationInput.jsx
+++ client/src/components/LocationInput.jsx
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
 
 class LocationInput extends Component {
+	
+// 	constructor(props) {
+// 		super(props)
+// 		this.state = {
+// 			locationValue: null,
+// 			locationLat: null,
+// 			locationLong: null,
+// 		}
+
+// 		this.handleLocationChange = this.handleLocationChange.bind(this);
+// 		this.handleLocationSubmit = this.handleLocationSubmit.bind(this);
+// 	}
+
+// 	handleLocationSubmit(event) {
+// 		event.preventDefault();
+
+// 		console.log(this.state.locationValue)
+
+// 		//converts address to lat and long for use in Dark Sky API
+// 		fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${ this.state.locationValue }&key=AIzaSyB_hFnwO_brmJWv01IMy5IaVBd8MJgSIZw
+// `)
+// 		.then((res) => {
+// 			return res.json()
+// 		})
+// 		.then((jsonRes) => {
+// 			console.log(jsonRes.results[0].geometry.location.lat);
+// 			console.log(jsonRes.results[0].geometry.location.lng);
+// 			this.setState({
+// 				locationLat: jsonRes.results[0].geometry.location.lat,
+// 				locationLong: jsonRes.results[0].geometry.location.lng,
+// 			})
+// 		})
+// 	}
+
+// 	handleLocationChange(event) {
+// 		this.setState({
+// 			locationValue: event.target.value,
+// 		})
+// 	}
+
 	render() {
 		return (
-			<form className="location_input">
+			<form className="location_input" onSubmit={ this.props.handleLocationSubmit } >
 				<div className="form-group">
 					<label htmlFor="address_input">Address</label>
-					<input type="text" name="location" placeholder="Enter Address Here" id="address_input" className="form-control" />
+					<input type="text" name="location" placeholder="Enter Address Here" id="address_input" className="form-control" onChange={ this.props.handleLocationChange } />
 				</div>	
 				<input className="btn btn-default" type="submit" value="Submit" />
 			</form>
